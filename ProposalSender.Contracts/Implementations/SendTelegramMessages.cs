9459080cd2b0a6ed4400ee3805c45d26f70fd2a8
Artur Sizov <artur-sa@mail.ru>
2023-03-15 07:33:32 +0300

    Добавил необходимые папки и проекты

---
+++ ProposalSender.Contracts/Implementations/SendTelegramMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WTelegram;
+
+namespace ProposalSender.Contracts.Implementations
+{
+    public class SendTelegramMessages
+    {
+        public readonly Client Client;
+
+        public SendTelegramMessages()
+        {
+            Client.
+        }
+    }
+}
